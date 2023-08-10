85c1c6d223ffeb20793dda63bf681436d9b36cc6
Datsomor <edemwilson123@gmail.com>
2023-08-10 07:30:23 -0700

    C Programming

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,2 @@
+#!/bin/bash
+main() { printf("Size of a char: %lu byte(s)\nSize of an int: %lu byte(s)\nSize of a long int: %lu byte(s)\nSize of a long long int: %lu byte(s)\nSize of a float: %lu byte(s)\n", sizeof(char), sizeof(int), sizeof(long int), sizeof(long long int), sizeof(float)); }
