0903715d9ad1319fcc3469312695fc3a10cf9566
Datsomor <edemwilson123@gmail.com>
2023-08-15 19:20:39 -0700

    working

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -14,8 +14,6 @@ void print_times_table(int n)
 {
 	{
 		int i, j, k;
-
-
 		if (n >= 0 && n <= 15)
 		{
 			for (i = 0; i <= n; i++)
@@ -53,4 +51,3 @@ void print_times_table(int n)
 			}
 		}
 	}
-
