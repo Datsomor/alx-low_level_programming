75bae5a0391f5ac1ef85c580917d5ffeb759730f
Datsomor <edemwilson123@gmail.com>
2023-08-15 18:24:53 -0700

    working

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,23 @@
 #include "main.h"
-	/**
-	 * main - print _putchar
-	 * is written by mbah
-	 * Return: 0
-	 */
 
-
-	int main(void)
-	{
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return (0);
-	}
-
+/**
+ * main - Entry point of the program.
+ *
+ * This function prints the characters of the string "_putchar" using
+ * the _putchar function, followed by a newline character.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    _putchar('_');
+    _putchar('p');
+    _putchar('u');
+    _putchar('t');
+    _putchar('c');
+    _putchar('h');
+    _putchar('a');
+    _putchar('r');
+    _putchar('\n');
+    return (0);
+}
